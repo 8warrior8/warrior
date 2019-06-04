b064e9778b4889b53bd8bd58227ffbfa0f70e56e
lihe2014 <lihe2014@boco.com.cn>
2019-06-04 14:16:14 +0800

    vuex

---
+++ src/store/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// 使用vuex
+Vue.use(Vuex)
+
+// vuex的标准写法
+const store = new Vuex.Store({
+	//vuex中的数据源，我们需要保存的数据就保存在这里
+	state:{ 
+		count: 1,
+		objList: [
+			{id: 1, text: 'obj1', status: true},
+			{id: 2, text: 'obj2', status: false},
+			{id: 3, text: 'obj3', status: true}
+		] 
+	},
+	//Getter相当于vue中的computed计算属性，getter 的返回值会根据它的依赖被缓存起来，
+	//且只有当它的依赖值发生了改变才会被重新计算，
+	//这里我们可以通过定义vuex的Getter来获取，Getters 可以用于监听、state中的值的变化，返回计算后的结果
+	getters:{
+		getStoreStateCount: function(state){ //上面的state
+			return state.count;
+		},
+		getTrueObjList: function(state){
+			return state.objList.filter(f=>f.status);
+		}
+	},
+	//修改store中的值唯一的方法就是提交mutation来修改
+	//一条重要的原则就是要记住 mutation 必须是同步函数
+	mutations: {
+		add(state, n){
+			if (!n) {
+				n = 1;
+			}
+			state.count = state.count + n;
+		},
+		remove(state, n){
+			if (!n) {
+				n = 1;
+			}
+			state.count = state.count - n;
+		},
+	},
+	//官方并不介意我们这样直接去修改store里面的值，而是让我们去提交一个actions，
+	//在actions中提交mutation再去修改状态值，先定义actions提交mutation的函数
+	//actions可以异步提交，完成后，根据状态，更新mutations
+	actions: {
+		addFunc(context, n){
+			context.commit("add", n);
+		},
+		removeFunc(context, n){
+			context.commit("remove", n);
+		}
+	}
+})
+
+export default store
