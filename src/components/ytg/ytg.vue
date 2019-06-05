9f55dd909693e9663ac7fb5918c80d6e70ac0ba0
yaotiangang <yaotiangang@boco.com.cn>
2019-06-05 15:27:53 +0800

    ytg提

---
+++ src/components/ytg/ytg.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="ytg">
+    <container>
+      <template v-slot:header>
+        <div class="my-link">
+          <func></func>
+        </div>
+      </template>
+      <template v-slot:default>
+        <router-view/>
+      </template>
+      <template v-slot:footer>
+        <span>低部内容</span>
+      </template>
+    </container>
+  </div>
+</template>
+
+<script>
+import container from "./container";
+import func from "./func";
+export default {
+  name: "ytg",
+  components: {
+    container: container,
+    func: func
+  }
+};
+</script>
+
+<style>
+</style>
