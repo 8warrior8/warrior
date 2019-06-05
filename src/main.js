cf7d3ec1d10e8268d15d44ce42a54b1c07097a6c
jiangshitao <jiangshitao@boco.com.cn>
2019-06-05 17:23:20 +0800

    test

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-import './styles/test.css'
 import './styles/global.styl'
 import './styles/globalLight.styl'
 
